ca7b8af75b905b14891671eb0fc8e99fb8136c06
Nick Nance <nance.nick@gmail.com>
2016-01-16 23:38:51 -0800

    Improve babel config

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-// by requiring `babel/register`, all of our successive `require`s will be Babel'd
-require('babel-register');
-require('./server.js');
+// by requiring `babel-register`, all of our successive `require`s will be Babel'd
+require('babel-register')({
+  extensions: ['.es6']
+});
+require('babel-polyfill');
+require('./server.es6');
